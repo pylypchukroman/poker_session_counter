3a602943b5cd7bb4de1763f187b61e6d293e9cc4
pylypchukroman <romanpylypchuk@gmail.com>
2025-12-12 20:39:59 +1100

    added tailwind

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -3,6 +3,7 @@ import { Header } from '../Components/Header';
 export const HomePage = () => {
   return (
     <>
+      <h1 className="text-2xl font-bold tracking-wider line-through">TEST</h1>
       <Header />
       <div>Home Page</div>
     </>
